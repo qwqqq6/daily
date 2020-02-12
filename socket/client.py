d4f5be76712f3feb1580d75245e3eb8196e6b4d4
qwqqq6 <ballonsgoo@gmail.com>
2020-02-13 03:34:42 +0800

    socket

---
+++ socket/client.py
@@ -0,0 +1,11 @@
+import socket
+
+Host = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((Host, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
